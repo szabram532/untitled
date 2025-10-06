1b8ce6409d34c7153d0bcebe4094b1fbbdbc7f0c
szabram532 <piotrszabram532@gmail.com>
2025-10-06 13:13:24 +0200

    a

--- PracaDomWyk2.java
+++ PracaDomWyk2.java
@@ -1,26 +1,25 @@
 import java.util.Scanner;
 
 public class PracaDomWyk2 {
-    static void pracaDomowaWyk2(){
+    static void pracaDomowaWyk2() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Podaj Liczbe a");
         int a = sc.nextInt();
         System.out.println("Podaj Liczbe b");
         int b = sc.nextInt();
-        pracaDomowaWyk2pomocnicza(a,b);
+        pracaDomowaWyk2pomocnicza(a, b);
     }
 
-    static void pracaDomowaWyk2pomocnicza(int a,int b){
+    static void pracaDomowaWyk2pomocnicza(int a, int b) {
         int c = a ^ b;
         int d = a & b;
-        d = d<< 1;
-        if (d==0){
+        d = d << 1;
+        if (d == 0) {
             IO.println(c);
-        }
-        else{
-            a=c;
-            b=d;
-            pracaDomowaWyk2pomocnicza(a,b);
+        } else {
+            a = c;
+            b = d;
+            pracaDomowaWyk2pomocnicza(a, b);
         }
     }
 }
